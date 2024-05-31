b416a61bc287298eb41ead22e1a1985124784c80
find11570 <find11570@naver.com>
2024-05-31 17:02:50 +0900

    [김현수] about, url 예외처리 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Error from '@/pages/ErrorPage.vue'
 import Home from '@/pages/Home.vue'
-// import About from '@/pages/About.vue'
+import About from '@/pages/AboutView.vue'
 import Members from '@/pages/Members.vue'
 import MembersInfo from '@/pages/MembersInfo.vue'
 // import Videos from '@pages/Videos.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: Home },
-    // { path: '/about', component: About},
-    { path: '/members', component: Members },
-    // { path: '/videos', component: Videos}
-    { path: '/members/:id', component: MembersInfo}
-  ]
+    history: createWebHistory(),
+    routes: [
+
+        { path: '/', component: Home },
+        { path: '/about', component: About },
+        { path: '/members', component: Members },
+        // { path: '/videos', component: Videos}
+        { path: '/members/:id', component: MembersInfo },
+        {
+            path: '/:catchAll(.*)',
+            component: Error
+        }
+    ]
 })
 
 export default router
