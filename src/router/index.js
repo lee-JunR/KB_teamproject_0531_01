674f64d34c2be40e9a297f4f5fcaa833856c7726
lee-junr <wns2610@icloud.com>
2024-05-31 16:14:44 +0900

    feat : Json 추가 및 멤버 목록 생성

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/pages/Home.vue'
 // import About from '@/pages/About.vue'
-// import Members from '@pages/Members.vue'
+import Members from '@/pages/Members.vue'
+import MembersInfo from '@/pages/MembersInfo.vue'
 // import Videos from '@pages/Videos.vue'
 
 const router = createRouter({
@@ -10,8 +11,9 @@ const router = createRouter({
   routes: [
     { path: '/', component: Home },
     // { path: '/about', component: About},
-    // { path: '/members', component: Members },
+    { path: '/members', component: Members },
     // { path: '/videos', component: Videos}
+    { path: '/members/:id', component: MembersInfo}
   ]
 })
 
